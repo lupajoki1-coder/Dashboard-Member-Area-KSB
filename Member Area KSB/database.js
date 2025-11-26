6bce48aed726e9a429e1db129a3650c11bc3abc3
lupajoki1-coder <lupajoki1@gmail.com>
2025-11-26 16:23:46 +0700

    database.js

--- Member Area KSB/database.js
+++ Member Area KSB/database.js
@@ -1,17 +1,18 @@
 // Data dari 'database pass klien.csv'
 // File ini berfungsi sebagai database lokal sederhana untuk aplikasi.
 const database = [
-  { "nomor": 1, "password": "krl2025", "url": "https://apl-dshbrd-halalcvkrl0670e1.netlify.app/" },
+  { "nomor": 1, "password": "krl2025", "url": "https://dashboard-progress-halal-cv-krl-moj.vercel.app/" },
   
   // -- CONTOH PENAMBAHAN PENGGUNA BARU --
   // Cukup salin format di bawah ini dan letakkan setelah baris terakhir, sebelum tanda `]`
   // Pastikan setiap baris objek diakhiri dengan koma, kecuali baris terakhir.
 
-  { "nomor": 2, "password": "btl2025", "url": "https://apl-dshbrd-btlrpob28b83d.netlify.app/" },
-  { "nomor": 3, "password": "ksb2025", "url": "https://apl-dshbrd-all-klien-ksb-01a.netlify.app/" },
-  { "nomor": 4, "password": "shn2025", "url": "https://halal-cvshn-kediri-01a.netlify.app/" },
-  { "nomor": 5, "password": "mck2025", "url": "https://momzilla-craft-kitchen-01a.netlify.app/" },
+  { "nomor": 2, "password": "btl2025", "url": "https://dashboard-proyek-pt-btl-proboliggo.vercel.app/" },
+  { "nomor": 3, "password": "ksb2025", "url": "https://dashboard-progress-proyek-all-klien.vercel.app/" },
+  { "nomor": 4, "password": "shn2025", "url": "https://dashboard-progress-halal-cv-shn-gil.vercel.app/" },
+  { "nomor": 5, "password": "mck2025", "url": "https://dashboard-progress-pt-momzilla-ck-m.vercel.app/" },
   { "nomor": 6, "password": "jws2025", "url": "https://jaya-wijaya-sejahtera-malang-001a.netlify.app/" }
   // Tambahkan pengguna lain di sini jika diperlukan...
 ];
+
 
